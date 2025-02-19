6ebb3eadc5d43c25dfd16dde04194c8a5ba44434
wings232 <a.jaheer786@gmail.com>
2025-02-19 16:42:53 +0530

    new files added with image logo

--- index.php
+++ index.php
@@ -6,14 +6,36 @@
     <title>Techshuttle | Home</title>    
     <link rel="stylesheet" href="css/reset.css"/>
     <link rel="stylesheet" href="css/root.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"/>
     <link rel="stylesheet" href="css/fonts.css"/>    
     <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
     <div class='container'> <!--container Starts -->
-        <div class='header'>
-            Hi
-        </div>
+        <div class='header_con'><!--header_con Starts -->
+            <div class="header_center"><!--header_center Starts -->
+                <div class="header"><!--header Starts -->
+                    <div class='logo_search'><!--logo_search Starts -->
+                        <div class='logo'><!--logo Starts -->
+                            <img src='images/logo.png'/>
+                        </div><!--logo Ends -->
+                        <div class='cate_m'>
+                            <div class='i_con'>
+                                <div class="icon">
+                                    <svg class="gb_F" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path><image src="https://ssl.gstatic.com/gb/images/bar/al-icon.png" alt="" height="10" width="24" style="border:none;display:none \9"></image></svg>
+                                </div>
+                                <div class='c_name'>
+                                    All Courses
+                                </div>
+                                <div class='arrow'>
+                                    <i class="fa fa-angle-down"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--logo_search Ends -->
+                </div><!--header Ends -->
+            </div><!--header_center Ends -->
+        </div><!--header_con Ends -->
     </div><!--container Ends -->
 </body>
 </html>
