c81ed16ef2955b2a73dc66a11371ce2ff27454b4
wings232 <a.jaheer786@gmail.com>
2025-02-19 18:07:35 +0530

    changed

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                         <div class='logo'><!--logo Starts -->
                             <img src='images/logo.png'/>
                         </div><!--logo Ends -->
-                        <div class='cate_m'>
+                        <div class='cate_m'><!--cate_m Starts -->
                             <div class='i_con'>
                                 <div class="icon">
                                     <svg class="gb_F" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path><image src="https://ssl.gstatic.com/gb/images/bar/al-icon.png" alt="" height="10" width="24" style="border:none;display:none \9"></image></svg>
@@ -29,6 +29,26 @@
                                 </div>
                                 <div class='arrow'>
                                     <i class="fa fa-angle-down"></i>
+                                </div>
+                            </div>
+                        </div><!--cate_m Ends -->
+                        <div class='search'>
+                            <div class='s_bar'>
+                                <div class='search_box'>
+                                    <input type='text' placeholder="What do you want to learn?"/>
+                                </div>
+                                <div class='button'>
+                                    <button class="search_submit" type="submit"><i class="fa fa-search"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='login'>
+                            <div class="login_b">
+                                <div class="image">
+                                    <i class='fas fa-user-alt'></i>
+                                </div>
+                                <div class="name">
+                                    Login
                                 </div>
                             </div>
                         </div>
