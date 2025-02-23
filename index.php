17d78306b6f6421273bffab90983f92967df6321
wings232 <a.jaheer786@gmail.com>
2025-02-23 18:44:09 +0530

    added

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Techshuttle | Home</title>    
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/root.css"/>
+    <link rel="stylesheet" href="css/fonts.css"/>    
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <div class='container'> <!--container Starts -->
+        <div class='header'>
+        </div>
+    </div><!--container Ends -->
+</body>
+</html>
