13d6e5e12e08c40479e953ef20844dd4d729728a
wings232 <a.jaheer786@gmail.com>
2025-02-28 18:02:13 +0530

    Index and style css changed

--- index.php
+++ index.php
@@ -537,6 +537,104 @@
             </div>
         </div><!--about_in_con Ends -->
 
+        
+        <div class="blog_t_con"><!--blog_t_con Starts -->
+            <div class="blog_t_center"><!--blog_t_center Starts -->
+                <div class="blog_t"><!--blog_t Starts -->
+                    <div class="blog_heads"><!--blog_heads Starts -->
+                        <div class="pop_course">
+                            <div class="pop_title">
+                                <div class="title">News & Blog</div>
+                            </div>
+                            <div class="pop_head">
+                                Our News 
+                                <span>& Blog 
+                                    <img  src="images/label/course_head_line.svg" alt="" />
+                                </span>
+                            </div>
+                        </div>
+                        <div class="blog_cate">
+                            <div class="banner-btn wow fadeup-animation animated" data-wow-duration="0.8s" data-wow-delay="0.5s" style="visibility: visible; animation-duration: 0.8s; animation-delay: 0.5s;">
+                                <a href="" class="sec-btn" title="Get Started"><span>View All Blogs</span></a>
+                            </div>
+                        </div>
+                    </div>  <!--blog_heads Ends -->
+                    <div class="image_blog">
+                        <ul>
+                            <li><!--Loop Starts -->
+                                <div class="image">
+                                    <div class="img_b">
+                                        <img src="images/label/01.jpg" alt="img">
+                                        <img src="images/label/01.jpg" alt="img">
+                                    </div>
+                                </div>
+                                <div class="img_cont">
+                                    <div class="date">
+                                        May 20, 2025
+                                    </div>
+                                    <div class="head">
+                                        The Power of Lifelong Learning Why Education Never Stops
+                                    </div>
+                                    <div class="read">
+                                        <a href="#">
+                                            Read More
+                                            <i class="fa fa-angle-double-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li><!--Loop Ends -->
+                            <li><!--Loop Starts -->
+                                <div class="image">
+                                    <div class="img_b">
+                                        <img src="images/label/02_1.webp" alt="img">
+                                        <img src="images/label/02_1.webp" alt="img">
+                                    </div>
+                                </div>
+                                <div class="img_cont">
+                                    <div class="date">
+                                        May 20, 2025
+                                    </div>
+                                    <div class="head">
+                                        The Power of Lifelong Learning Why Education Never Stops
+                                    </div>
+                                    <div>
+                                        <a href="#">
+                                            Read More
+                                            <i class="fa fa-angle-double-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li><!--Loop Ends -->
+                            <li><!--Loop Starts -->
+                                <div class="image">
+                                    <div class="img_b">
+                                        <img src="images/label/01.jpg" alt="img">
+                                        <img src="images/label/01.jpg" alt="img">
+                                    </div>
+                                </div>
+                                <div class="img_cont">
+                                    <div class="date">
+                                        May 20, 2025
+                                    </div>
+                                    <div class="head">
+                                        The Power of Lifelong Learning Why Education Never Stops
+                                    </div>
+                                    <div>
+                                        <a href="#">
+                                            Read More
+                                            <i class="fa fa-angle-double-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li><!--Loop Ends -->
+                        </ul>
+                    </div>
+                </div><!--blog_t Starts -->
+            </div><!--blog_t_center Starts -->
+        </div><!--blog_t_con Starts -->
+
+
+
 
     <div class="certi_cor_con"><!--certi_cor_con Starts -->
         <div class="certi_cor_center">
