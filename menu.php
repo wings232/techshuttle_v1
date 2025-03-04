873001643eefef061bb9e43391511dd927163549
wings232 <a.jaheer786@gmail.com>
2025-03-04 18:02:12 +0530

    menu added

---
+++ menu.php
@@ -0,0 +1,110 @@
+<div class='overlay'>
+
+</div>      
+<div class="menu_bar">
+    <i class="fa fa-bars" aria-hidden="true"></i>                     
+</div>
+<div class='i_con'>
+    <div class="icon">
+        <svg class="gb_F" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path><image src="https://ssl.gstatic.com/gb/images/bar/al-icon.png" alt="" height="10" width="24" style="border:none;display:none \9"></image></svg>
+    </div>
+    <div class='c_name'>
+        All Courses
+    </div>
+    <div class='arrow'>
+        <i class="fa fa-angle-down"></i>
+    </div>
+</div>
+<div class="tech_menu">                                
+    <div class="cate_sub">
+        <ul>
+            <li>SAP  <!--Menu First Child Starts -->
+                <div class="sub_cate">
+                    <div class="sub_level">
+
+                        <div class="course_head">
+
+                            <div class="head">
+                                <a href="course_catalogue.php?key_one=courses&key_two=technical">Technical</a>
+                            </div>
+                            <div class="head_list">
+                                <ul>
+                                    <li>
+                                        <a href="courses/technical">SAP Basis</a>
+                                    </li>
+                                    <li>
+                                        <a href="courses/sap-abap">SAP Abap</a>
+                                    </li>                                                                
+                                </ul>
+                            </div>
+                        </div>    
+                        <div class="course_head">
+
+                            <div class="head">
+                                <a href="course_catalogue.php?key_one=courses&key_two=technical">Functional</a>
+                            </div>
+                            <div class="head_list">
+                                <ul>
+                                    <li>
+                                        <a href="courses/technical">SAP FICO</a>
+                                    </li>
+                                    <li>
+                                        <a href="courses/sap-abap">SAP SD</a>
+                                    </li>                                                                
+                                </ul>
+                            </div>
+                        </div>    
+                        <div class="course_img">
+                            <img src="images/menu_image/course_bar_image3.webp">
+                        </div>
+                    
+                    </div>
+                </div>    
+            </li><!--Menu First Child Ends -->
+            <li>Workday  <!--Menu First Child Starts -->
+                <div class="sub_cate">
+                    <div class="sub_level">
+
+                        <div class="course_head">
+
+                            <div class="head">
+                                <a href="course_catalogue.php?key_one=courses&key_two=technical">Technical</a>
+                            </div>
+                            <div class="head_list">
+                                <ul>
+                                    <li>
+                                        <a href="courses/technical">Workday Basis</a>
+                                    </li>
+                                    <li>
+                                        <a href="courses/sap-abap">SAP Abap</a>
+                                    </li>                                                                
+                                </ul>
+                            </div>
+                        </div>    
+                        <div class="course_head">
+
+                            <div class="head">
+                                <a href="course_catalogue.php?key_one=courses&key_two=technical">Functional</a>
+                            </div>
+                            <div class="head_list">
+                                <ul>
+                                    <li>
+                                        <a href="courses/technical">SAP FICO</a>
+                                    </li>
+                                    <li>
+                                        <a href="courses/sap-abap">SAP SD</a>
+                                    </li>                                                                
+                                </ul>
+                            </div>
+                        </div>    
+                        <div class="course_img">
+                            <img src="images/menu_image/course_bar_image3.webp">
+                        </div>
+                    
+                    </div>
+                </div>    
+            </li><!--Menu First Child Ends -->
+            
+        </ul>
+    </div>
+</div>
