3e8f3ba8a41ba18e34ced456f79d1072ea4c0de3
wings232 <a.jaheer786@gmail.com>
2025-08-28 09:49:15 +0530

    new files added

---
+++ about_head.php
@@ -0,0 +1,123 @@
+<div class="about_in_center">
+                <div class="right_course">
+                    <div class="rig_course">
+                        <div class="course_bg">
+                            <div class="image">
+                                <img src="images/right_01.png" alt="img">
+                            </div>
+                            <div class="cta__content-three">
+                                <div class="content__left">
+                                    <h2 class="title">Finding Your Right Courses</h2>
+                                    <p>intuitive shared inbox makes it easy for team member</p>
+                                </div>
+                                <div class="banner-btn wow fadeup-animation animated" data-wow-duration="0.8s" data-wow-delay="0.5s" style="visibility: visible; animation-duration: 0.8s; animation-delay: 0.5s;">
+                                    <a href="contact-us.html" class="sec-btn" title="Get Started"><span>Get Started</span></a>
+                                </div>
+                            </div>
+                            <div class="cta__shape-two">
+                                <img src="images/svg/shape_k.svg" alt="shape">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="about_in">
+                    <div class="about_image">
+                        <div class="img">
+                            <div class="img_left">
+                                <img src="images/label/01.webp"/>
+                                <img src="images/label/02_1.webp"/>
+                            </div>
+                            <div class="img_right">
+                                <img src="images/label/03.webp"/>
+                            </div>
+
+                            <img class="shape_one" src="images/svg/01_about.svg"/>
+                            <img class="shape_two" src="images/svg/02_about.svg"/>
+                            <img class="shape_three" src="images/svg/03_about.svg"/>                              
+
+                        </div>           
+                    </div>
+                    <div class="about_content"> 
+                        <div class="about_c">
+                            <div class="about_head">
+                                <div class="head_one">
+                                    Know about Techshuttle
+                                </div>
+                                <div class="head_two">
+                                    We providing the best online courses
+                                </div>
+                                <div class="lines">
+                                    <div class="line_one"></div>
+                                    <div class="line_two"></div>
+                                </div>
+                            </div>
+                            <div class="about_para">
+                                <p>
+                                    TECHSHUTTLE was founded by two corporate visionaries driven by a profound insight into the barriers facing technology training, dedicated to breaking down obstacles and nurturing a new generation of tech enthusiasts.
+                                </p>
+                               
+                            </div>
+
+                            <!-- <div class="ab_item">
+                                <ul>
+                                    <li>
+                                        <div class="check"><i class="fa fa-check"></i></div>
+                                        <div class="check_txt">Embarking on a Mission</div>
+                                    </li>
+                                    <li>
+                                        <div class="check"><i class="fa fa-check"></i></div>
+                                        <div class="check_txt">Guiding Light</div>
+                                    </li>
+                                    <li>
+                                        <div class="check"><i class="fa fa-check"></i></div>
+                                        <div class="check_txt">Empowering Education</div>
+                                    </li>
+                                    <li>
+                                        <div class="check"><i class="fa fa-check"></i></div>
+                                        <div class="check_txt">Building Futures</div>
+                                    </li>
+                                </ul>
+                            </div> -->
+
+                            <section class="accordion accordion--radio">
+                                <div class="tab">
+                                    <input type="radio" name="accordion-2" id="rd1">
+                                    <label for="rd1" class="tab__label">Embarking on a Mission</label>
+                                    <div class="tab__content">
+                                    <p>With fervent zeal, we set forth to democratize technology, making it accessible to all at an affordable cost.</p>
+                                    </div>
+                                </div>
+                                <div class="tab">
+                                    <input type="radio" name="accordion-2" id="rd2">
+                                    <label for="rd2" class="tab__label">Guiding Light</label>
+                                    <div class="tab__content">
+                                    <p>Our mission extends beyond technology; we counsel and empower students with meaningful information, illuminating the path to a brighter future.</p>
+                                    </div>
+                                </div>
+                                <div class="tab">
+                                    <input type="radio" name="accordion-2" id="rd3">
+                                    <label for="rd3" class="tab__label">Empowering Education</label>
+                                    <div class="tab__content">
+                                    <p>Striving to bridge gaps, we equip students with tools and knowledge, paving the way for a transformative educational journey.</p>
+                                    </div>
+                                </div>
+                                <div class="tab">
+                                    <input type="radio" name="accordion-2" id="rd4">
+                                    <label for="rd4" class="tab__label">Building Futures</label>
+                                    <div class="tab__content">
+                                    <p>From humble beginnings, we embark on a journey to shape futures, providing not just technology, but a roadmap to success for every aspiring learner.</p>
+                                    </div>
+                                </div>
+                                <!-- <div class="tab">
+                                    <input type="radio" name="accordion-2" id="rd3">
+                                    <label for="rd3" class="tab__close">Close open tab &times;</label>
+                                </div> -->
+                            </section>
+
+                            <!-- <div class="banner-btn wow fadeup-animation animated" data-wow-duration="0.8s" data-wow-delay="0.5s" style="visibility: visible; animation-duration: 0.8s; animation-delay: 0.5s;">
+                                <a href="contact-us.html" class="sec-btn" title="Get Started"><span>Get Started</span></a>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ about_us.php
@@ -0,0 +1,225 @@
+<?php
+	include "dbconn.php";
+	include_once "func_templates/func_code.php";
+	include "check_ses_list.php";
+	//include "constantconfig.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Techshuttle | Home</title>    
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/root.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/fonts.css"/>    
+    <link rel="stylesheet" href="css/animation.css"/> 
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/about/about.css"/>
+    <script type="text/javascript" src="js/jquery.js"></script>
+</head>
+<body>
+    <div class='container'> <!--container Starts -->
+        <div class='header_con'><!--header_con Starts -->
+            <?php include "header.php"; ?>
+        </div><!--header_con Ends -->
+
+        <section class="about_banner"><!--about_banner Starts -->
+            <div class='about_one'><!--about_one Starts -->
+                <div class="column"><!--column Starts -->
+                    <div class='wrapper'><!--wrapper Starts -->
+                        <div class='ab_sha_one'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_01.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+                        <div class='ab_sha_two'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_02.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+                        <div class='ab_img_one'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/ab_img_01.webp" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+                        <div class='ab_sha_three'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_03.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+                         <div class='ab_sha_four'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_04.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+                         <div class='ab_sha_five'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_01.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+
+                        <div class='ab_img_two'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/ab_img_02.webp" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+
+                        <div class='ab_sha_six'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_05.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+
+                        <div class='ab_sha_seven'><!--ab_sha_one Starts -->
+                            <div class='custom_shp'>
+                                <div class='shp_row'>
+                                    <div class='image'>
+                                        <i class="fa fa-phone"></i>
+                                    </div>
+                                    <div class='content'>
+                                        <div class='c_head'>
+                                            Online Support
+                                        </div>
+                                        <div class='c_phone'>
+                                            <a href="tel:+918870802060">+91 8870802060</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!--ab_sha_one Ends -->
+
+                        <div class='ab_sha_eight'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/about_06.png" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+
+                        <div class='ab_img_three'><!--ab_sha_one Starts -->
+                           <span>
+                                <img decoding="async" src="images/label/ab_img_03.webp" alt="">
+                           </span> 
+                        </div><!--ab_sha_one Ends -->
+                        <div class='ab_text'><!--ab_text Starts -->
+                            <div class='text_title'>
+                                The Best Program to Enroll <br> for Exchange
+                            </div>
+                        </div><!--ab_text Ends -->
+
+                        <div class='ab_text_two'><!--ab_text_two Starts -->
+                            <div class='text_title'>
+                                We are driven by <span>Pure PASSION !</span>
+                            </div>
+                        </div><!--ab_text_two Ends -->
+
+                        <div class='ab_sha_nine'><!--ab_sha_nine Starts -->
+                           <span class='scroll_d'>
+                                <i class="fa fa-long-arrow-down"></i>
+                           </span> 
+                        </div><!--ab_sha_nine Ends -->
+                    </div><!--wrapper Ends -->
+                </div><!--column Ends -->
+            </div><!--about_one Ends -->
+        </section><!--about_banner Ends -->
+        
+        <div class="about_in_con"><!--about_in_con Starts -->
+            <?php include "about_head.php"; ?>       
+        </div><!--about_in_con Ends -->
+
+         <div class="fac_life_con">
+            <div class="fac_life_center">
+                <div class="fac_life">
+                    <div class="fac_left"><!--fac_left Starts -->
+                        <div class="img">
+                            <img src="images/image_01.png">
+                        </div>
+                        <!-- <div class="img_one">
+                            <img src="images/banner/fac_02.png"/>
+                        </div>
+                        <div class="img_two">
+                            <img src="images/banner/fac_03.png"/>
+                        </div>
+                        <div class="img_three">
+                            <img src="images/banner/fac_04.png"/>
+                        </div>
+                        <div class="img_four">
+                            <img src="images/banner/fac_05.png"/>
+                        </div> -->
+                    </div><!--fac_left Ends -->
+                    <div class="fac_right">
+                        <div class="title">
+                            <div class="head">
+                                Challenges makes you productive.
+                            </div>
+                           
+                        </div>
+                        <div class="info">
+                            <ol>
+                                <li>
+                                    <span>01</span>
+                                    <div class='po_tech'>
+                                        <p>Expert Guidance</p>
+                                        <p>Our founders, seasoned corporate leaders, bring their vision and expertise to Techshuttle, ensuring every student is guided along the right path to success in the tech industry.</p>
+                                    </div>
+                                </li>
+                                
+                                <li>
+                                    <span>02</span>
+                                    <div class='po_tech'>
+                                        <p>Lifetime Support</p>
+                                        <p>At Techshuttle, our commitment doesn't end at graduation. We're here for our students as returning alumni, offering ongoing support and mentorship as they navigate their careers in technology.</p>
+                                    </div>
+                                </li>
+                                
+                                <li>
+                                    <span>03</span>
+                                    <div class='po_tech'>
+                                        <p>Beacon of Direction</p>
+                                        <p>In a world where many forget to show the right path, Techshuttle shines as a beacon, illuminating the way forward for aspiring technologists with unparalleled dedication and care.</p>
+                                    </div>
+                                </li>
+                                
+                               
+                            </ol>
+                        </div>
+                        
+                      
+                         
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class='coundown_con'><!--coundown_con Starts -->
+            <div class='coundown_center'><!--coundown_center Starts -->
+                <div class='coundown'><!--coundown Starts -->
+                    <div class='count_left'>
+                        <div class='head_sub'>
+                            Fun Facts
+                        </div>
+                        <div class='head_main'>
+                            Creating Impact <mark>Around The World</mark>
+                        </div>
+                        <div class='para'>
+                            <p>
+                                With our global catalog spanning the latest skills and topics, people and organizations everywhere are able to adapt to change and thrive.
+                            </p>
+                        </div>
+                    </div>
+                    <div class='count_right'>
+                        
+                    </div>
+                </div><!--coundown Ends -->
+            </div><!--coundown_center Ends -->
+        </div><!--coundown_con Ends -->
+
+        <div class="footer_con"><!--footer_con Starts -->
+            <?php include "footer.php"; ?>  
+        </div><!--footer_con Ends -->
+
+    </div><!--container Ends -->
+</body>
+</html>
+<script type="text/javascript" src="js/code.js"></script>
